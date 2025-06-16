6d79ca5d2405cbec886dea33be2bd8b2adf3724d
ARYAL KATKAR <aryalkatkar@gmail.com>
2025-06-16 16:17:30 +0530

    Update test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,7 +4,7 @@ def test_add():
     assert add(2,3)==5
     assert add(3,3)==6
     assert add(4,3)==7
-    assert add(5,3)==7
+    assert add(5,3)==8
     
 def test_sub():
     assert sub(2,3)==-1
